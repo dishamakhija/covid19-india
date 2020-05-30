a1996168d635b32e62475ef00e20e7ad7f338985
anupama.agarwal <anuag@amazon.com>
2020-05-30 09:48:41 +0530

    enabled SEIHRD for scenario forecasting

--- covid19-library/src/driver.py
+++ covid19-library/src/driver.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     # ForecastingModule.from_config_file("../config/sample_forecasting_config.json")
     # ModelEvaluator.from_config_file("../config/sample_evaluation_config.json")
     # TrainingModule.from_config_file("../config/sample_training_config.json")
-    ScenarioForecastingModule.from_config_file("../config/sample_scenario_forecasting_config.json")
+    # ScenarioForecastingModule.from_config_file("config/sample_npi_scenario_forecasting_config.json")
+    ScenarioForecastingModule.from_config_file("../config/sample_param_override_scenario_forecasting_config.json")
--- covid19-library/src/model_wrappers/model_factory.py
+++ covid19-library/src/model_wrappers/model_factory.py
@@ -1,5 +1,6 @@
 from entities.model_class import ModelClass
 from model_wrappers.base import ModelWrapperBase
+from model_wrappers.intervention_enabled_seihrd import InterventionEnabledSEIHRD
 from model_wrappers.intervention_enabled_seir import InterventionEnabledSEIR
 from model_wrappers.seir import SEIR
 from model_wrappers.seihrd import SEIHRD
@@ -20,5 +21,7 @@ class ModelFactory:
     def get_intervention_enabled_model(model_class: ModelClass, model_parameters):
         if model_class.__eq__(ModelClass.SEIR):
             return InterventionEnabledSEIR(model_parameters)
+        if model_class.__eq__(ModelClass.SEIHRD):
+            return InterventionEnabledSEIHRD(model_parameters)
         else:
             raise Exception("Model Class is not in supported classes {}".format(["SEIR"]))
